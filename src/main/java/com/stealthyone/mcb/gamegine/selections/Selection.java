8617b7ece1612514db981aaeca9c28783e078ec0
Stealth2800 <stealth2800@stealthyone.com>
2014-02-02 22:06:59 -0800

    Converted 'Selection' to an interface.

--- src/main/java/com/stealthyone/mcb/gamegine/selections/Selection.java
+++ src/main/java/com/stealthyone/mcb/gamegine/selections/Selection.java
@@ -1,65 +1,34 @@
 package com.stealthyone.mcb.gamegine.selections;
 
-import com.stealthyone.mcb.stbukkitlib.lib.utils.LocationUtils;
-import org.bukkit.block.Block;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.Location;
 
-public class Selection {
+public interface Selection {
 
-    private Block block1;
-    private Block block2;
+    /**
+     * Sets the minimum point for the selection.
+     *
+     * @param minPoint Minimum point to set.
+     */
+    public void setMinimumPoint(Location minPoint);
 
-    public Selection() {
-        block1 = null;
-        block2 = null;
-    }
+    /**
+     * Sets the maxmium point for the selection.
+     *
+     * @param maxPoint Maxmium point to set.
+     */
+    public void setMaximumPoint(Location maxPoint);
 
-    public Selection(ConfigurationSection config) {
-        try {
-            block1 = LocationUtils.stringToLocation(config.getString("block1")).getBlock();
-        } catch (NullPointerException ex) {
-            block1 = null;
-        }
+    /**
+     * Clears the selection.
+     */
+    public void clearSelection();
 
-        try {
-            block2 = LocationUtils.stringToLocation(config.getString("block2")).getBlock();
-        } catch (NullPointerException ex) {
-            block2 = null;
-        }
-    }
-
-    public void save(ConfigurationSection config) {
-        config.set("block1", LocationUtils.locationToString(block1.getLocation(), true));
-        config.set("block2", LocationUtils.locationToString(block2.getLocation(), true));
-    }
-
-    public Block getBlock1() {
-        return block1;
-    }
-
-    public Block getBlock2() {
-        return block2;
-    }
-
-    public void setBlock1(Block block1) {
-        this.block1 = block1;
-    }
-
-    public void setBlock2(Block block2) {
-        this.block2 = block2;
-    }
-
-    public void clearSelection() {
-        block1 = null;
-        block2 = null;
-    }
-
-    public boolean areBothPointsSet() {
-        return !(getBlock1() == null || getBlock2() == null);
-    }
-
-    public boolean areBlocksInDifferentWorlds() {
-        return !getBlock1().getWorld().getName().equals(getBlock2().getWorld().getName());
-    }
+    /**
+     * Checks to see if the selection is valid.
+     *
+     * @return True if selection is valid.
+     *         False if selection is not valid.
+     */
+    public boolean isValid();
 
 }
